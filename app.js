0ccd892c86dd5072ff1369068045e9b6f33fd3f4
afeiship <1290657123@qq.com>
2017-02-18 13:34:46 +0800

    update foreach

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let fs = require('fs');
 let cheerio = require('cheerio');
 let superagent = require('superagent');
+var request = require("co-request");
 let co = require('co');
+var each = require('co-each');
 let pageUrls = [
     'http://sc.chinaz.com/yinxiao/PianTouYinXiao.html',
     // 'http://sc.chinaz.com/yinxiao/PianTouYinXiao_2.html',
@@ -13,33 +15,55 @@ let pageUrls = [
 
 let targetMusicUrls = [];
 
+function* getPageUrl(inUrl) {
+    let urls = [];
+    let response = yield request(inUrl);
+    let $ = cheerio.load(response.body);
+    $('.music_block').each(function (index, val) {
+        urls.push(
+            $(val).find('.z a').attr('href')
+        );
+    });
+    return urls;
+}
+
+co(function* () {
+    let urls = yield getPageUrl(pageUrls);
+
+    yield pageUrls.forEach(function*(url){
+        urls= urls.concat(yield getPageUrl(url));
+    });
+
+    console.log('xx', urls.length)
+})
+
 
 
 //http://sc.chinaz.com/yinxiao/PianTouYinXiao_2.html
 
-let getPageUrl = co.wrap(function* () {
-    return yield superagent
-        .get(url)
-        .end(function (err, res) {
-            let targetUrls = [];
-            let $ = cheerio.load(res.text);
-            console.log($('.music_block').length)
+// let getPageUrl = co.wrap(function* () {
+//     return yield superagent
+//         .get(url)
+//         .end(function (err, res) {
+//             let targetUrls = [];
+//             let $ = cheerio.load(res.text);
+//             console.log($('.music_block').length)
 
-            $('.music_block').each(function (index, val) {
-                targetUrls.push(
-                    $(val).find('.z a').attr('href')
-                );
-            });
+//             $('.music_block').each(function (index, val) {
+//                 targetUrls.push(
+//                     $(val).find('.z a').attr('href')
+//                 );
+//             });
 
-            return new Promise().resolve({targetUrls});
-        });
-});
+//             return new Promise().resolve({targetUrls});
+//         });
+// });
 
 
-co(function* () {
-    let urls = yield this.getPageUrl();
-    console.log(urls);
-})
+// co(function* () {
+//     let urls = yield this.getPageUrl();
+//     console.log(urls);
+// })
 
 
 // pageUrls.forEach(function (url) {
